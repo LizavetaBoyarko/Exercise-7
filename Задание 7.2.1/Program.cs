3bebd20f87fb4ca42b70b99ad517836a512cd98f
Елизавета Боярко <LizOK_boarko@mail.ru>
2021-11-09 23:17:13 +0300

    добавлено 7.2

---
+++ Задание 7.2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба:");
+            Console.Write("l=");
+            int l = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            CalcParam(l, out S, out V);
+            Console.WriteLine(S);
+            Console.WriteLine(V);
+            Console.ReadKey();
+        }
+        static void CalcParam(int l, out double S, out double V)
+        {
+            S = 6 * (Math.Pow(l, 2));
+            V = Math.Pow(l, 3);
+        }
+    }
+}
